f5f38d63b2406d39125e1e1c2856332a20e37309
Manki <ranger.ahilles@gmail.com>
2024-10-05 14:38:33 +0300

    updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import gamelistscraper "/gamelistScraper"
+import (
+	"fmt"
+	"log"
+	"main/db"
+	freegamesscraper "main/freeGamesScraper"
+	listscraper "main/listScraper"
+)
 
 func main() {
-	gamelistscraper.ParseGameList()
+	Db, err := db.NewEditDb()
+	if err != nil {
+		panic(err)
+	}
+	listscraper.ParseGameList(Db)
+	freeGameName, isGameInList, err := freegamesscraper.CheckFreeGame()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(freeGameName, isGameInList, err)
+
 }
